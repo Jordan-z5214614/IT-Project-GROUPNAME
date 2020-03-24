53fef07d3e63b49643046ffc7fad4976caaf28f4
Luke Batten <z3420772@ad.unsw.edu.au>
2020-03-24 01:39:43 +0000

    Continued dev work for modbus - successfully have a server, and a master that can communicate with the slave device

--- review1_demo/modbus_client.py
+++
@@ -1,7 +0,0 @@
-from pymodbus.client.sync import ModbusTcpClient
-
-client = ModbusTcpClient('localhost', 5020)
-client.write_coil(1, True)
-result = client.read_coils(1,1)
-print(result.bits[0])
-client.close()
