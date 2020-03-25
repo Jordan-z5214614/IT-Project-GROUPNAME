672d6792e47f3e3885ad5d6f0c0e028c4f48df29
Parrot User <user@localhost.localdomain>
2020-03-25 00:00:42 -0400

    Completed dev work on Modbus server, slave, master. Added bash script to launch server/slave.
    Slave controls a motor based on values in register 1. Master runs a UI and changes the values of the
    register. Server runs the Modbus protocol.

---
+++ review1_demo/demo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./modbus_server.py &
+./modbus_slave.py &
+#./modbus_logger &
--- review1_demo/modbus_master.py
+++ review1_demo/modbus_master.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
+
+#Import Libraries
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 
 import time
 import sys
 
+#Setup Modbus Connection
 client = ModbusClient('localhost', port=5020)
-UNIT=0x1
+UNIT=0x1        #Address of the slave to control
 
 client.connect()
 
+#Main Loop
 while True:
-    client.write_coil(1, True, unit=UNIT)
-    time.sleep(1)
-    client.write_coil(1, False, unit=UNIT)
-    time.sleep(1)
+    speed = input("Enter a speed from -1 to 1: \n")     #Takes user input
+    speed = int((speed*10)+10)                          #Modbus only accepts positive ints, so we convert to and int between 0-20
+    client.write_register(1, speed, unit=UNIT)
+
+
