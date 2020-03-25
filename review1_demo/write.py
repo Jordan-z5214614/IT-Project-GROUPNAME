732df54414f1c92fb886bea42cb6891b64cf524e
z5206300 <56392744+z5206300@users.noreply.github.com>
2020-03-25 15:40:26 +1100

    Update write.py

--- review1_demo/write.py
+++ review1_demo/write.py
@@ -3,26 +3,59 @@ from sqlite3 import Error
 
 
 
-def writeSensorFeedback(time, output, input):
-	connect()
+def writeSensorFeedback(conn, time, output, input):
+	cur = conn.cursor()
 	cur.execute("INSERT INTO sensorFeedback (timestamp, outputPMWValue, originalInput) VALUES (?, ?, ?)", (time, output, input))
 
-def writeSensorFeedin(date, hour, input)
-	connect()
+def writeSensorFeedin(conn, date, hour, input):
+	cur = conn.cursor()
 	cur.execute("INSERT INTO sensorFeedin (currentDate, currentHour, inputPMWValue) VALUES (?, ?, ?)", (date, hour, input))
 
-def writeHMIStoredData(idNum, yourName, user, password, type)
-	connect()
+def writeHMIStoredData(conn, idNum, yourName, user, password, type):
+	cur = conn.cursor()
 	cur.execute("INSERT INTO HMIStoredData (id, name, userName, PasswordHashed, UserType) VALUES (?, ?, ?, ?, ?)", (idNum, yourName, user, password, type))
 
-def writeWCNStoredData(idNum, yourName, user, password, type)
-	connect()
+def writeWCNStoredData(conn, idNum, yourName, user, password, type):
+	cur = conn.cursor()
 	cur.execute("INSERT INTO WCNStoredData (id, name, userName, PasswordHashed, aliasSystemID) VALUES (?, ?, ?, ?, ?)", (idNum, yourName, user, password, alias))
 
-
-
-
-
+def readSensorFeedback(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM sensorFeedback")
+ 
+    rows = cur.fetchall()
+ 
+    for row in rows:
+        print(row)
+	
+def readSensorFeedin(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM sensorFeedin")
+ 
+    rows = cur.fetchall()
+ 
+    for row in rows:
+        print(row)
+	
+def readSensorFeedin(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM sensorFeedin")
+ 
+    rows = cur.fetchall()
+ 
+    for row in rows:
+        print(row)
+	
+	
+def readSensorFeedin(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM sensorFeedin")
+ 
+    rows = cur.fetchall()
+ 
+    for row in rows:
+        print(row)
+	
 
 def connect()
 	database = r"C:\sqlite\db\pythonsqlite.db"
