51e44726baa9684430dc35374a3281ca214c8aa2
Parrot User <user@localhost.localdomain>
2020-04-07 04:47:29 -0400

    Continued dev work on PLC Driver. Implemented basic PLCLogic class for testing. Implemented motorDriver for live testing on Pi

--- prototype1/PLC/PLCLogic.py
+++ prototype1/PLC/PLCLogic.py
@@ -1,3 +1,15 @@
+import time
+
 def main(device_list,param_list,writeModbus,readModbus):
-    device_list['dev0'].setPwm("test")
-    print(device_list['dev1'].getRpm())
+
+    try:
+        while True:
+            time.sleep(0.5)
+            targetRPM = param_list.get('targetrpm')
+            print(targetRPM)
+            if (targetRPM != 0):
+                device_list.get('dev0').setPwm(1,1)
+            else:
+                device_list.get('dev0').setPwm(1,0)
+    except KeyboardInterrupt:
+        device_list.get('dev0').setPwm(0)
