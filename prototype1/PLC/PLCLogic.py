906e0ec61c043ab2c3c20a4857aed9074d47289c
z3420772 <z3420772@ad.unsw.edu.au>
2020-04-07 19:07:00 +1000

    More bug squashing

--- prototype1/PLC/PLCLogic.py
+++ prototype1/PLC/PLCLogic.py
@@ -6,10 +6,9 @@ def main(device_list,param_list,writeModbus,readModbus):
         while True:
             time.sleep(0.5)
             targetRPM = param_list.get('targetrpm')
-            print(targetRPM)
             if (targetRPM != 0):
                 device_list.get('dev0').setPwm(1,1)
             else:
                 device_list.get('dev0').setPwm(1,0)
     except KeyboardInterrupt:
-        device_list.get('dev0').setPwm(0)
+        device_list.get('dev0').setPwm(1,0)
