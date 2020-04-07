d8653e787a9da5c81e0d1d920ec6af0a6689f49b
z3420772 <z3420772@ad.unsw.edu.au>
2020-04-07 19:04:25 +1000

    Squashed bugs in motorDriver

--- review1_demo/modbus_server.py
+++ review1_demo/modbus_server.py
@@ -112,7 +112,7 @@ def run_server():
     # run the server you want
     # ----------------------------------------------------------------------- #
     # Tcp:
-    StartTcpServer(context, identity=identity, address=("localhost", 5020))
+    StartTcpServer(context, identity=identity, address=("0.0.0.0", 5020))
 
     # TCP with different framer
     # StartTcpServer(context, identity=identity,
