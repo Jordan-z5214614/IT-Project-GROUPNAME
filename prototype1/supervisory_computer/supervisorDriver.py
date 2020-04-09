16831df3a3e0450b035d165176adac287072e175
Luke Batten <z3420772@student.adfa.edu.au>
2020-04-09 12:53:05 +1000

    Updated PLC Driver to use absolute path for config file

--- prototype1/supervisory_computer/supervisorDriver.py
+++ prototype1/supervisory_computer/supervisorDriver.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python3
 #TODO Flesh out user inputs. Configure to take registers/addresses from a config file
 
-#Import modbus server from pymodbus
+#Import modbus protocol 
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-
+#Import modbus server driver
+import modbusServer
 #Import python libs
 from multiprocessing import Process
-import modbusServer
+import paramiko
+import configparser
 import sys
 import time
+
+#Connects using SSH and launches PLCDriver on the passed PLC unit
+def plc_start(hostname,username,password):
+
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys()
+    ssh.connect(hostname=hostname,username=username,password=password)
+    ssh_stdin = ssh.exec_command('./IT-Project-GROUPNAME/prototype1/PLC/PLCDriver.py')
 
 #Starts a server in a thread, then takes a PWM value from the user. 
 def main():
 
     server = Process(target=modbusServer.run_server)
 
+    #Read in config file
+    config = configparser.RawConfigParser()
+    file = r'config.txt'
+    config.read(file)
+
+
     try:
         server.start()
+
+        for plc in config.items('plc list'):
+
+            username = config.get(plc[0],'username')
+            password = config.get(plc[0],'password')
+            hostname = plc[1]
+
+            plc_start(hostname,username,password)
 
         client = ModbusClient('supevisor',5020)
         client.connect()
@@ -26,7 +50,7 @@ def main():
             client.write_register(0,int(pwm),unit=0) #register address and device address are hard coded for testing. Will be dynamic from config file in end product
 
     except KeyboardInterrupt:
-        print("Shutting down")
+        print("\nShutting down")
         server.terminate()
 
 
