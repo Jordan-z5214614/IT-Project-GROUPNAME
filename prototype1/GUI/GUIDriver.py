04db1ec6353ed15e0bced5855407b710774b0dd3
Luke <z3420772@adfa.edu.au>
2020-05-30 04:11:03 -0400

    Comments for everyone

--- prototype1/GUI/GUIDriver.py
+++ prototype1/GUI/GUIDriver.py
@@ -10,42 +10,50 @@ import time
 import multiprocessing
 import configparser
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+
+IP='111.220.27.216'
+USER='pi'
+PWD='gr0upn@m3'
+
+# ---------------------------------------------------------------- #
+# Defines the parameters that can be used by PyQt signal processes
+# to send signals outside the main GUI loop
+# TODO update these so that parameters are generated dynamically.
+# Update will always exist, the others need to be generated.
+# ---------------------------------------------------------------- #
 class HandlerSignals(Qt.QObject):
     rpm = Qt.pyqtSignal(int)
     update = Qt.pyqtSignal()
     power = Qt.pyqtSignal(int)
-class ModbusHandler(Qt.QThread):
-    IP='111.220.27.216'
-    USER='pi'
-    PWD='gr0upn@m3'
-    PLC_dict = {} # dictionary for storing the PLC information
-    PLC_count = 0 # number of plcs in use
-    
-    #function to find the number of PLCs in use, using the config file, putting this information into the PLC-dict
-    def find_num_PLCs(self):
-        ssh = paramiko.SSHClient()
-        ssh.load_system_host_keys()
-        ssh.connect(self.IP,username=self.USER,password=self.PWD)
 
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('cat IT-Project-GROUPNAME/prototype1/supervisory_computer/config.txt')
-        config = configparser.RawConfigParser()
-        config.read_string(config_cat.read().decode('ascii').strip('\n'))
-        i = 1
-        for plc in config.items('plc list'):
-            PLC_dict['plc'+i] = 'turbine' + i
-            i = i + 1
-        PLC_count = len(PLC_dict)
+# ---------------------------------------------------------------- #
+# Handles the back end modbus integration for the GUI app
+# ---------------------------------------------------------------- #
+class ModbusHandler(Qt.QThread):
 
     def __init__(self,turbine1,turbine2):
         super(ModbusHandler, self).__init__()
+
+        #Takes the turbines passed and creates local objects
         self.turbine1 = turbine1
         self.turbine2 = turbine2
+        #Creates a PLC config dict to take in the SSH info. TODO move this process into parent
         self.plc_configs = {}
+        #Create signal handler object
         self.signals = HandlerSignals()
+        #Start the supervisor process
         self.startSupervisor()
+        #Start the modbus local client
         self.startModbusClient()
+    #Override deafult PyQt run method
     @Qt.pyqtSlot()
+    #Main signal handler loop
     def run(self):
+        # ------------------------------------------------------------ #
+        # Gets data from only turbine 1 at this stage, then reads/writes
+        # to Modbus using the signals, and then calls update to update
+        # the GUI, then sleeps
+        # ------------------------------------------------------------ #
         while True:
             data = self.turbine1.getValues()
             self.writeModbus(data)
@@ -81,7 +89,6 @@ class ModbusHandler(Qt.QThread):
 
             self.plc_configs.update({plc[1]:plc_config})
 
-        print(self.plc_configs)
         #Starts the supervisor
         ssh.exec_command('python3 IT-Project-GROUPNAME/prototype1/supervisory_computer/supervisorDriver.py')
         time.sleep(5)
@@ -91,45 +98,82 @@ class ModbusHandler(Qt.QThread):
     def startModbusClient(self):
         self.client = ModbusClient(self.IP,5020)
         self.client.connect()
+
+# ------------------------------------------------------------------------------------------ #
+# Main GUI class
+# Builds a base window that the GUI classes for the different types of devices can be loaded
+# onto
+# ------------------------------------------------------------------------------------------ #
 class GUI:
 
-    
     def __init__(self):
+
+        #Loads the base window
         self.buildGUI()
+
+        #Loads the threadpool and handlers to process user inputs
         self.threadpool = Qt.QThreadPool()
         handler = ModbusHandler(self.turbine1,self.turbine2)
         handler.signals.rpm.connect(self.turbine1.setRPM)
         handler.signals.power.connect(self.turbine1.setPower)
         handler.signals.update.connect(self.turbine1.update)
         handler.start()
+
+        #Start the GUI loop
         self.window.show()
         self.app.exec_()
+
+        #After the window closes clear Modbus and kill the handler thread
         handler.writeModbus([0,0,0,0,0])
         handler.terminate()
+
     def buildGUI(self):
+
+        #Setup PyQt window
         self.app = Q.QApplication([])
         self.window = Q.QWidget()
 
+        # ----------------------------------------------------------------- #
+        # Creates the windows for each supervisory computer. At this stage,
+        # is only set up for a single supervisor and is hard coded. 
+        # ----------------------------------------------------------------- #
         self.createSupervisorBox("1")
 
+        #Adds supervisor to layout
         layout = Q.QGridLayout()
         layout.addWidget(self.supervisorBox)
 
+        #Adds layout to the window
         self.window.setLayout(layout)
 
     def createSupervisorBox(self,number):
+
+        #Creates sub-box for supervisory computer
         self.supervisorBox = Q.QGroupBox("Supervisory Computer " + number)
 
+        #Sets up layout
         layout = Q.QGridLayout()
+
+        # ------------------------------------------------------------------ #
+        # Creates two turbine objects. 
+        # TODO - Change the code so this process is dynamic using plc_config
+        # ------------------------------------------------------------------ #
         self.turbine1 = Turbine.Turbine()
-        layout.addWidget(self.turbine1.createTurbineBox("1"),0,0)
         self.turbine2 = Turbine.Turbine()
+
+        #Adds created device windows to layout
+        layout.addWidget(self.turbine1.createTurbineBox("1"),0,0)
         layout.addWidget(self.turbine2.createTurbineBox("2"),0,1)
 
+        #Adds layout to window
         self.supervisorBox.setLayout(layout)
 
 if __name__=='__main__':
+
+    #Starts the login window
     login = Q.QApplication(sys.argv)
     window = TurbineLogin.Ui()
     login.exec_()
+
+    #Starts the main GUI program
     gui = GUI()
